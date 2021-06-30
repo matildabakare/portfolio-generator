a489c2b3e89bffc17bcabda74d28313df621f86c
Matilda Bakare <matildabakare@gmail.com>
2021-06-30 07:19:53 -0400

    added code to generate index.html

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
-    // for (let i = 0; i < profileDataArr.length; i++) {
-    //     console.log(profileDataArr[i]);
-    // } OR
-    // profileDataArr.forEach((profileItem) => {
-    //     console.log(profileItem)
-    // }); OR
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
