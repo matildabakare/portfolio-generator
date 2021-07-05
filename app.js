f49a9bbe34bf236ed6b497f91e3ccd64fd35457c
Matilda Bakare <matildabakare@gmail.com>
2021-07-05 02:24:37 -0400

    updated html template

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -134,11 +136,16 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+
 
 // const pageHTML = generatePage(name, github);
 
